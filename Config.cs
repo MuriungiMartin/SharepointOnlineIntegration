f8066b1c7b52b84854ffa48c720feeba3971d8f8
muriungiMartin <muriungimmwiti@gmail.com>
2023-10-24 03:16:48 +0300

    Add project files.

---
+++ Config.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace SharepointOnlineIntegration
+{
+    public class Config
+    {
+        public struct DefaultResponse
+        {
+            public int status { get; set; }
+            public int resNumber { get; set; }
+            public bool res { get; set; }
+            public string message { get; set; }
+            public string return_value { get; set; }
+            public string retvalue { get; set; }
+            public string dashvalue { get; set; }
+
+            public object data { get; set; }
+
+
+
+
+
+            public DefaultResponse(int status, int resNumber, string message, object data = null, string return_value = null, bool res = false, string dashvalue = null)
+            {
+                this.status = status;
+                this.resNumber = resNumber;
+                this.message = message;
+                this.data = data;
+                this.return_value = return_value;
+                this.retvalue = return_value;
+                this.dashvalue = dashvalue;
+                this.res = res;
+
+
+
+
+            }
+            public struct JSONArrayResponse
+            {
+                public int status { get; set; }
+                public JArray response { get; set; }
+
+
+
+                public JSONArrayResponse(int status, JArray message)
+                {
+                    this.status = status;
+                    this.response = message;
+                }
+            }
+        }
+    }
+}
---
+++ Controllers/SharepointController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.SharePoint.Client;
+using Microsoft.SharePoint.Authentication;
+using System.Security;
+using System.Net.NetworkInformation;
+using System.Net;
+
+namespace SharepointOnlineIntegration.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SharepointController : ControllerBase
+    {
+        public IConfiguration configuration = new ConfigurationBuilder()
+.SetBasePath(Directory.GetCurrentDirectory())
+.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+.Build();
+
+        [HttpGet("ListItems")]
+        public  IActionResult GetListItems(string listName)
+        {
+            Config.DefaultResponse response = new Config.DefaultResponse();
+
+            try
+            {
+                //string connectionString = configuration.GetConnectionString("DefaultConnection");
+                var siteUrl = configuration["Sharepoint:SiteUrl"];
+                var userName = configuration["Sharepoint:UserName"];
+                var password = configuration["Sharepoint:Password"];
+                var domain = configuration["Sharepoint:Domain"];
+                var securePassword = new SecureString();
+                foreach (char c in password)
+                {
+                    securePassword.AppendChar(c);
+                }
+                var onlineCredentials = new  NetworkCredential(userName, securePassword);
+                using (var clientContext = new ClientContext(siteUrl))
+                {
+                    clientContext.Credentials = onlineCredentials;
+                    var web = clientContext.Web;
+                    var list = web.Lists.GetByTitle(listName);
+                    var items = list.GetItems(CamlQuery.CreateAllItemsQuery());
+                    clientContext.Load(items);
+                    clientContext.ExecuteQuery();
+                    var result = new List<object>();
+                    foreach (var item in items)
+                    {
+                        result.Add(new
+                        {
+                            Id = item.Id,
+                            Title = item["Title"]
+                        });
+                    }
+                    return Ok(new Config.DefaultResponse(200, 1, "Success", result));
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new Config.DefaultResponse(500, 0, ex.Message));
+            }
+        }
+
+        public void ConnectToSharePointOnline(string siteCollUrl, string userName, string password)
+        {
+
+            //Namespace: It belongs to Microsoft.SharePoint.Client
+            ClientContext ctx = new ClientContext(siteCollUrl);
+
+            // Namespace: It belongs to System.Security
+            SecureString secureString = new SecureString();
+            password.ToList().ForEach(secureString.AppendChar);
+
+            // Namespace: It belongs to Microsoft.SharePoint.Client
+            ctx.Credentials = new NetworkCredential(userName, secureString);
+
+            // Namespace: It belongs to Microsoft.SharePoint.Client
+            Site mySite = ctx.Site;
+
+            ctx.Load(mySite);
+            ctx.ExecuteQuery();
+
+            Console.WriteLine(mySite.Url.ToString());
+        }
+
+        //Upload file to SharePoint Online Document Library
+        public void UploadFileToSharePointOnline(string siteCollUrl, string userName, string password, string sourceFilePath, string targetLibraryPath)
+        {
+            // Namespace: It belongs to Microsoft.SharePoint.Client
+            ClientContext ctx = new ClientContext(siteCollUrl);
+            // Namespace: It belongs to System.Security
+            SecureString secureString = new SecureString();
+            password.ToList().ForEach(secureString.AppendChar);
+            // Namespace: It belongs to Microsoft.SharePoint.Client
+            ctx.Credentials = new NetworkCredential(userName, secureString);
+            // Namespace: It belongs to Microsoft.SharePoint.Client
+            Web web = ctx.Web;
+            // Namespace: It belongs to Microsoft.SharePoint.Client
+            FileCreationInformation newFile = new FileCreationInformation();
+            //Get the file name from source file path
+            string fileName = System.IO.Path.GetFileName(sourceFilePath);
+            //Assign to content byte[] i.e. documentStream
+            newFile.Content = System.IO.File.ReadAllBytes(sourceFilePath);
+            //Allow owerwrite of document
+            newFile.Overwrite = true;
+            //Upload URL
+            newFile.Url = siteCollUrl + targetLibraryPath + fileName;
+            //Upload document
+            Microsoft.SharePoint.Client.File uploadFile = web.GetFolderByServerRelativeUrl(targetLibraryPath).Files.Add(newFile);
+            //Update the metadata for a field having name "DocType"
+            uploadFile.ListItemAllFields["DocType"] = "Sample Document";
+            //Update the metadata for a field having name "Project"
+            uploadFile.ListItemAllFields["Project"] = "SharePoint Online Development";
+            //Update the metadata for a field having name "DocumentType"
+            uploadFile.ListItemAllFields["DocumentType"] = "Word Document";
+            //Update the metadata for a field having name "DocumentDescription"
+            uploadFile.ListItemAllFields["DocumentDescription"] = "How to Upload a document to SharePoint Online Document Library";
+            //Update the metadata for a field having name "DocumentOwner"
+            uploadFile.ListItemAllFields["DocumentOwner"] = "TSInfo Technologies";
+            //Update the metadata for a field having name "DocumentStatus"
+            uploadFile.ListItemAllFields["DocumentStatus"] = "Draft";
+            //Update the metadata for a field having name "DocumentID"
+            uploadFile.ListItemAllFields["DocumentID"] = "TSInfo-DOC-0001";
+        }
+    }
+
+}
+
